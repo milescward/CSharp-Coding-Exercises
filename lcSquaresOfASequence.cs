97a373edad36a966aab2ea90c41431a86d6e20dd
milescward <milesward@Miless-MacBook-Air.local>
2019-10-17 21:39:14 -0700

    quick update on Squares of a Sequence

--- lcSquaresOfASequence.cs
+++ lcSquaresOfASequence.cs
@@ -1,5 +1,12 @@
 using System;
+/*Given an array of integers A sorted in 
+non-decreasing order, return an array of 
+the squares of each number, also in sorted 
+non-decreasing order. */
 
+/* Memory Usage: 39.1 MB, less than 100.00% 
+of C# online submissions for Squares of a 
+Sorted Array.*/
 namespace CSharp_Practice
 {
     public class lcSquaresOfASequence
