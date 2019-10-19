052d6dd215bfd76c4baf69a968adbc8f4a8e4f42
milescward <milesward@Miless-MacBook-Air.local>
2019-10-18 20:39:40 -0700

    Adding UniqueMorseCodeRepresentations.cs

---
+++ AlphaToMorse.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Practice
+{
+    public class AlphaToMorse
+    {
+        /*International Morse Code defines a standard 
+        encoding where each letter is mapped to a 
+        series of dots and dashes, as follows: "a" 
+        maps to ".-", "b" maps to "-...", "c" maps 
+        to "-.-.", and so on. */
+
+        //faster than 80% of submissions
+
+        public int UniqueMorseRepresentation(string[] words)
+        {
+            HashSet<string> hash = new HashSet<string>();
+            Dictionary<char, string> MCDict = GetMCodeDictionary();
+            for (int i = 0; i < words.Length; i++)
+            {
+                List<string> mCodeBuilder = new List<string>();
+                string word = words[i];
+                foreach (var item in word)
+                {
+                    mCodeBuilder.Add(MCDict[item]);
+                }
+                string wordCode = string.Join("", mCodeBuilder);
+                hash.Add(wordCode);
+                mCodeBuilder.Clear();
+            }
+            return hash.Count;
+        }
+
+        private static Dictionary<char, string> GetMCodeDictionary()
+        {
+            //char c = 'A';
+            List<string> mCode = new List<string>() { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+            char[] c = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            Dictionary<char, string> myDict = new Dictionary<char, string>();
+            for (int i = 0; i < c.Length; i++)
+            {
+                myDict.Add(c[i], mCode[i]);
+            }
+            return myDict;
+        }
+    }
+}
