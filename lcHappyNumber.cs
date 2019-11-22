f4a66394241aae003bc4276795bc22226d1c7c6d
miles.c.ward@gmail.com <miles.c.ward@gmail.com>
2019-11-21 22:44:38 -0800

    Happy Number more efficient

--- lcHappyNumber.cs
+++ lcHappyNumber.cs
@@ -3,6 +3,18 @@ using System.Collections.Generic;
 
 namespace CSharp_Practice
 {
+    /*Write an algorithm to determine if a number is "happy".
+
+    A happy number is a number defined by the following process: 
+    Starting with any positive integer, replace the number by the 
+    sum of the squares of its digits, and repeat the process until 
+    the number equals 1 (where it will stay), or it loops endlessly
+     in a cycle which does not include 1. Those numbers for which 
+     this process ends in 1 are happy numbers. */
+
+
+    // O(log n) runtime
+    // O(log n) space
     public class lcHappyNumber
     {
         public bool IsHappy(int n)
@@ -24,5 +36,31 @@ namespace CSharp_Practice
             }
             return false;
         }
+
+        // O(log n) runtime
+        // O(1) space
+        public bool IsHappy2(int n) 
+        {
+            int slow = n;
+            int fast = NextNum(n);
+            while (slow != fast && fast != 1)
+            {
+                slow = NextNum(slow);
+                fast = NextNum(NextNum(fast));
+            }
+            return fast == 1;
+        }
+    
+    public int NextNum(int n)
+    {
+        double num = n;
+        double sum = 0;
+        while(num != 0)
+        {
+            sum += Math.Pow((num % 10), 2);
+            num = Math.Floor(num / 10);
+        }
+        return (int)sum;
+    }
     }
 }
