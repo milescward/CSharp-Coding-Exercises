8c59ef7bc6b9f3c368a5514c88c4011fd470a8e6
miles.c.ward@gmail.com <miles.c.ward@gmail.com>
2019-11-23 00:56:12 -0800

    Update to FindTownJudge

--- lcFindTownJudge.cs
+++ lcFindTownJudge.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CSharp_Practice
 {
@@ -9,19 +10,52 @@ namespace CSharp_Practice
     {
         public int FindJudge(int N, int[][] trust) 
         {
-            var d = new Dictionary<int, int>();
-            foreach (var pair in trust)
+            var d = new Dictionary<int, List<int>>();
+            foreach (var rel in trust)
             {
-                d.Add(pair[0], pair[1]);
+                if (d.ContainsKey(rel[0]))
+                    d[rel[0]].Add(rel[1]);
+                else
+                    d.Add(rel[0], new List<int>{rel[1]});
             }
-            for (int i = 0; i < N; i++)
+            if (d.Keys.Count == N)
+                return -1;
+            var keyHash = d.Keys.ToHashSet();
+            var valHash = new HashSet<int>();
+            foreach (var rel in d.Values)
             {
-                if (!d.ContainsKey(i))
+                foreach (var item in rel)
                 {
-                    return i;
+                    valHash.Add(item);
                 }
             }
-            return -1;
+            var possJudges = new List<int>();
+            foreach (var item in valHash)
+            {
+                if (keyHash.Add(item) == true)
+                {
+                    possJudges.Add(item);
+                }
+            }
+            if(possJudges.Count == 0)
+                return -1;
+                for (int i = 0; i < possJudges.Count; i++)
+                {
+                    foreach (var item in d.Values)
+                    {
+                        if (item.Contains(possJudges[i]))
+                        {
+                            possJudges.RemoveAt(i);
+                        }
+                    }
+                }
+            
+            if (possJudges.Count == 1)
+                return possJudges[0];
+            else
+            {
+                return -1;
+            }
         }
     }
 }
