28ca17dd072b41a29b0f30347c2882b64a03c42d
miles.c.ward@gmail.com <miles.c.ward@gmail.com>
2019-11-27 20:11:24 -0800

    faster

--- lcSetMatrixZeros.cs
+++ lcSetMatrixZeros.cs
@@ -51,5 +51,51 @@ namespace CSharp_Practice
             }
             return m;
         }
+
+        public void SerZeroes2(int[][] matrix)
+        {
+            // if(matrix == null || matrix.Length == 0)
+            //     return;
+            bool first = false;
+            int row = matrix.Length;
+            int col = matrix[0].Length;
+            
+            for(var i = 0; i < row; i++)
+            {
+                if(matrix[i][0] == 0)
+                    first = true;
+                for(var j = 1; j < col; j++)
+                {
+                    if(matrix[i][j] == 0)
+                    {
+                        matrix[i][0] = 0;
+                        matrix[0][j] = 0;
+                    }
+                }
+            }
+
+            for (int i = 1; i < row; i++)
+                for (int j = 1; j < col; j++)
+                {
+                    if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                        matrix[i][j] = 0;
+                }
+            
+            if (matrix[0][0] == 0)
+            {
+                for (int i = 0; i < col; i++)
+                {
+                    matrix[0][i] = 0;
+                }
+            }
+
+            if(first)
+            {
+                for(int i = 0; i < row; i++)
+                {
+                    matrix[i][0] = 0;
+                }
+            }
+        }
     }
 }
